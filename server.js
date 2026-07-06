2848c851f0e43b16ca0a3cd115ba7eb1f04e4bea
bdalalybwstt46-art <bdalalybwstt46@gmail.com>
2026-07-06 17:04:26 +0100

    Change server port from 3000 to 30222

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.get('/pair', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 30222;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
